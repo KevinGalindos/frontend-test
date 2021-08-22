8fe6b5b1f4d6eeb8cf5f6079ee8dba0b15b7584e
Julio-Vasquez <jualvalitube@gmail.com>
2021-08-22 18:52:33 -0500

    feat(props): add props and proptypes

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,3 +1,17 @@
-export const Loading = () => {
-  
+import { string } from 'prop-types'
+
+export const Loading = ({ message }) => {
+  return (
+    <div>
+      {message}
+    </div>
+  )
+}
+
+Loading.propTypes = {
+  message: string.isRequired
+}
+
+Loading.defaultProps = {
+  message: 'Cargando...'
 }
