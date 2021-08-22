001776c36b0e28fe4bd8a58668ce2682d7f1a999
Julio-Vasquez <jualvalitube@gmail.com>
2021-08-22 18:53:34 -0500

    feat(routes): urls

--- src/routes/Private.js
+++ src/routes/Private.js
@@ -1,3 +1,19 @@
+import { Router, Redirect } from '@reach/router'
+import { Suspense, lazy } from 'react'
+
+import { Loading } from '../components/Loading'
+
+const Posts = lazy(()=> import('./../views/Private/Posts'))
+
 export const Private = () => {
-  
+  return (
+    <Suspense fallback={
+      <Loading message="Cargando contenido"/>
+    }>
+      <Router>
+        <Posts path="/" />
+        <Redirect from="*" to="/" noThrow />
+      </Router>
+    </Suspense>
+  )
 }
--- src/routes/Public.js
+++ src/routes/Public.js
@@ -1,3 +1,23 @@
-export const Public = () => {
+import { Router, Redirect } from '@reach/router'
+import { Suspense, lazy } from 'react'
 
+import { Loading } from './../components/Loading'
+
+const Home = lazy(()=> import("./../views/Public/Home"))
+const Login = lazy(()=> import("./../views/Public/Login"))
+const Signup = lazy(()=> import("./../views/Public/Signup"))
+
+export const Public = () => {
+  return (
+    <Suspense fallback={
+      <Loading message="Cargando vistas"/>
+    }>
+      <Router>
+        <Home path="/" />
+        <Login path="/login" />
+        <Signup path="signup" />
+        <Redirect from="*" to="/" noThrow />
+      </Router>
+    </Suspense>
+  )
 }
