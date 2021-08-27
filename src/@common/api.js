291c8546b4321c7044ceab311d1dfa070b63d867
Julio-Vasquez <jualvalitube@gmail.com>
2021-08-26 20:05:47 -0500

    feat(api): method get

--- src/@common/api.js
+++ src/@common/api.js
@@ -2,32 +2,45 @@ import Store from './../store'
 import { logout } from './../services/Auth'
 import { BASE_URL_API } from './environment'
 
-export const GET = (url, params) => {}
+export const GET = async (url, params) => {
+  const newUrl = new URL(`${BASE_URL_API}/${url}`)
+
+  if (params)
+    Object.keys(params).forEach(
+      key => newUrl.searchParams.append(key, params[key])
+    )
+  return fetch(newUrl.toString(), {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    }
+  })
+    .then(async (res) => {
+      if (res.status === 401) {
+        Store.dispatch(logout())
+        return res
+      }
+      res.payload = await res.json()
+      return res
+    })
+    .catch(error => error)
+};
 
 export const POST = async (url, body, header) => {
-  return Promise.race([
-    fetch(`${BASE_URL_API}/${url}`, {
+  console.log(url)
+  console.log(body)
+  return fetch(`${BASE_URL_API}/${url}`, {
       method: "POST",
       headers: header
         ? header
         : {
             Accept: "application/json",
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            "Access-Control-Allow-Origin": "https://javascript.info"
           },
       body: JSON.stringify(body),
-    }),
-    new Promise((_resolve, reject) =>
-      setTimeout(
-        () =>
-          reject({
-            payload: {
-              error: "TIMEOUT",
-            }
-          }),
-        28000
-      )
-    )
-  ])
+    })
     .then(async res => {
       if (res.status === 401) { 
         Store.dispatch(logout())
